6a38dbc7ee41a2e7069cd1e40307f9bf5f03edd0
bloodydark <justiceremain48694802@gmail.com>
2021-01-23 15:57:39 +0900

    progate基礎編

--- Greeting.py
+++ Greeting.py
@@ -1,10 +1,16 @@
-a = 'a'
-print(f'a is {a}')
- 
-x, y, z = 1, 2, 3
-print(f'a is {x}, {y}, {z}')
-print(f'a is {z}, {y}, {x}')
- 
-name = 'Jun'
-family = 'Sakai'
-print(f'My name is {name} {family}. Watashi ha {family} {name}')
+items = {'apple': 100, 'banana': 200, 'orange': 400}
+for item_name in items:
+    print('--------------------------------------------------')
+    print(item_name + 'は1個' + str(items[item_name]) + '円です')
+    
+    # inputを用いて入力を受け取り、変数input_countに代入してください
+    input_count =  input("購入する"+ item_name +"の個数を入力してください：")
+    # キーと変数input_countを用いて「購入する◯◯の個数は△△個です」となるように出力してください
+    print("購入する"+item_name +"の個数は"+ str(input_count) +"個です")
+    
+    # input_countを数値として変数countに代入してください
+    count = int(input_count)
+    # 変数total_priceに果物1個の値段と変数countを掛けた値を代入してください
+    total_price = items[item_name] * count
+    # 変数total_priceと型変換を用いて、「支払い金額は◯◯円です」となるように出力してください
+    print("支払い金額は"+str(total_price)+"円です")
